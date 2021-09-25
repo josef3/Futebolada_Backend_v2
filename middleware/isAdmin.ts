077cb4d35fbd11df61dac775574983b4f8ef44d6
josef3 <josefigueiredo12@gmail.com>
2021-09-25 23:55:17 +0100

    refactor: :fire: Remove useless middleware

--- middleware/isAdmin.ts
+++
@@ -1,8 +0,0 @@
-import { IRequest, IResponse, INext } from "../interfaces";
-
-export const isAdmin = (req: IRequest, res: IResponse, next: INext) => {
-    if (req.isAdmin !== true) {
-        return res.sendStatus(403);
-    }
-    next();
-}
