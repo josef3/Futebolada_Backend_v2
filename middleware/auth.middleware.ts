1290e3ce2dd3c17195cb37a8e4a4575c35155cbf
josef3 <josefigueiredo12@gmail.com>
2021-09-28 16:12:30 +0100

    refactor: :art: Change middleware files name to example.middleware.ts

--- middleware/auth.middleware.ts
+++ middleware/auth.middleware.ts
@@ -1,13 +1,18 @@
 import jwt from 'jsonwebtoken';
+import InvalidAuthTokenException from '../exceptions/InvalidAuthTokenException';
 import { IRequest, IResponse, INext } from '../interfaces';
 
 export function authenticateToken(req: IRequest, res: IResponse, next: INext) {
     const authHeader = req.headers.authorization;
     const token = authHeader && authHeader.split(' ')[1];
-    if (!token) return res.sendStatus(401);
+    if (!token) {
+        return next(new InvalidAuthTokenException());
+    }
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET || '', (error, user) => {
-        if (error) return res.sendStatus(401);
+    jwt.verify(token, String(process.env.ACCESS_TOKEN_SECRET), (error, user) => {
+        if (error) {
+            return next(new InvalidAuthTokenException());
+        }
 
         if (user) {
             if (user.id_admin > 0) {
