7ac8a37bc7ddbf96ac360cbf0969d01356f1ceb9
josef3 <josefigueiredo12@gmail.com>
2021-09-28 22:45:11 +0100

    refactor: :fire: Remove UsernameNotFoundException

--- exceptions/PlayerUsernameNotFoundException.ts
+++
@@ -1,9 +0,0 @@
-import HttpException from './HttpException';
-
-class PlayerUsernameNotFoundException extends HttpException {
-    constructor(username: string) {
-        super(404, `O jogador com o username ${username} não existe`);
-    }
-}
-
-export default PlayerUsernameNotFoundException;
