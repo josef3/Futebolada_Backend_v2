98ddeceb2d90183dcde8ca4516d37d945281dce5
josef3 <josefigueiredo12@gmail.com>
2021-09-30 13:24:13 +0100

    refactor: :art: Make use of error handling in middleware

--- middleware/isSuperAdmin.middleware.ts
+++ middleware/isSuperAdmin.middleware.ts
@@ -1,8 +1,9 @@
 import { IRequest, IResponse, INext } from "../interfaces";
+import NotAuthorizedException from '../exceptions/NotAuthorizedException';
 
 export const isSuperAdmin = (req: IRequest, res: IResponse, next: INext) => {
     if (req.admin_role !== 'admin') {
-        return res.sendStatus(403);
+        return next(new NotAuthorizedException());
     }
     next();
 }
