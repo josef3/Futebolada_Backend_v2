a24439e90d67a19077f1d7c6a271c4551734d285
josef3 <josefigueiredo12@gmail.com>
2021-10-04 18:38:08 +0100

    refactor: :art: Minimal refactor

--- exceptions/IdNotFoundException.ts
+++ exceptions/IdNotFoundException.ts
@@ -2,7 +2,7 @@ import HttpException from './HttpException';
 
 class IdNotFoundException extends HttpException {
     constructor(id: string, entity: string) {
-        super(404, `O ${entity} com o id ${id} não existe`);
+        super(404, `O/A ${entity} com o id ${id} não existe`);
     }
 }
 
