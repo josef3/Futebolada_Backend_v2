5b48aa459bda103ea77d19131a63a7179199c85f
josef3 <josefigueiredo12@gmail.com>
2021-10-05 10:56:52 +0100

    fix: :bug: Add missing middlewares

--- routes/weeks.ts
+++ routes/weeks.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 
+import { authenticateToken } from '../middleware/auth.middleware';
+import { isSuperAdmin } from '../middleware/isSuperAdmin.middleware';
+
 import { getWeekById, getWeeks, createWeek } from '../controllers/week';
 
 const router = express.Router();
 
 router.get('/', getWeeks);
 router.get('/:id', getWeekById);
-router.post('/', createWeek);
+router.post('/', authenticateToken, isSuperAdmin, createWeek);
 
 export default router;
